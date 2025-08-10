e4a959fb5acfe42370377bd3f23da2845f134254
Kirya <kkodolov40@gmail.com>
2025-08-10 23:09:57 +0300

    Добавление метода getSortedTasks для получения задач на странице

--- src/main/java/org/workswap/core/services/TaksService.java
+++ src/main/java/org/workswap/core/services/TaksService.java
@@ -1,10 +1,14 @@
 package org.workswap.core.services;
 
+import java.util.List;
+
 import org.workswap.datasource.admin.model.Task;
 
 public interface TaksService {
 
     Task findTask(String param);
+
+    List<Task> getSortedTasks(String sort, String type, String status);
 
     void createTask();
     void deleteTask();
--- src/main/java/org/workswap/core/services/impl/TaskServiceImpl.java
+++ src/main/java/org/workswap/core/services/impl/TaskServiceImpl.java
@@ -1,10 +1,17 @@
 package org.workswap.core.services.impl;
 
+import java.util.Comparator;
+import java.util.List;
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.workswap.datasource.admin.model.Task;
 import org.workswap.datasource.admin.repository.TaskRepository;
+import org.workswap.datasource.central.model.User;
+import org.workswap.datasource.central.repository.UserRepository;
 import org.workswap.common.enums.SearchModelParamType;
+import org.workswap.common.enums.TaskStatus;
+import org.workswap.common.enums.TaskType;
 import org.workswap.core.services.TaksService;
 import org.workswap.core.services.components.ServiceUtils;
 
@@ -17,6 +24,7 @@ public class TaskServiceImpl implements TaksService {
 
     private final TaskRepository taskRepository;
     private final ServiceUtils serviceUtils;
+    private final UserRepository userRepository;
 
     private Task findTaskFromRepostirory(String param, SearchModelParamType paramType) {
         switch (paramType) {
@@ -33,6 +41,60 @@ public class TaskServiceImpl implements TaksService {
     public Task findTask(String param) {
         SearchModelParamType paramType = serviceUtils.detectParamType(param);
         return findTaskFromRepostirory(param, paramType);
+    }
+
+    @Override
+    public List<Task> getSortedTasks(String sort, String type, String status) {
+
+        List<Task> tasks = taskRepository.findAll();
+
+        if (type != null) {
+            tasks.removeIf(task -> task.getTaskType() != TaskType.valueOf(type));
+        }
+
+        if (status != null) {
+            tasks.removeIf(task -> task.getStatus() != TaskStatus.valueOf(status));
+        }
+
+        for (Task task : tasks) {
+            User executor = null;
+
+            if (task.getExecutorId() != null) {
+                executor = userRepository.findById(task.getExecutorId()).orElse(null);
+            }
+
+            User author = null;
+
+            if (task.getExecutorId() != null) {
+                author = userRepository.findById(task.getAuthorId()).orElse(null);
+            }
+
+            task.setExecutor(executor);
+            task.setAuthor(author);
+        }
+
+        Comparator<Task> comparator;
+
+        switch (sort) {
+            case "created":
+                comparator = Comparator.comparing(Task::getCreatedAt, Comparator.nullsFirst(Comparator.naturalOrder()));
+                break;
+            case "deadline":
+                comparator = Comparator.comparing(Task::getDeadline, Comparator.nullsLast(Comparator.naturalOrder()));
+                break;
+            case "name":
+                comparator = Comparator.comparing(Task::getName, String.CASE_INSENSITIVE_ORDER);
+                break;
+            case "status":
+                comparator = Comparator.comparing(Task::getStatus);
+                break;
+            default:
+                comparator = Comparator.comparing(Task::getId); // сортировка по id по умолчанию
+        }
+
+        tasks.sort(comparator);
+
+        return tasks;
     }
 
     @Override
